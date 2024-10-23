4285bb98220a389b3a1ef87f4fe9ce618b17ab3e
Pankaj <pyadav5Visteon@gmail.com>
2024-10-23 11:03:20 +0530

    2nd module

---
+++ dictionaryEx.py
@@ -0,0 +1,3 @@
+#dictionary have the key pair value, we can access the key and pair saperately
+d={"john":40, "peter":45}
+print(list(d.keys())) #this will print list of keys
---
+++ setExp.py
@@ -0,0 +1,3 @@
+# as set preserve order and holds unique element 
+nums = set([1,1,2,3,3,3,4,4])
+print(len(nums)) #so lenth of the nums is 4
