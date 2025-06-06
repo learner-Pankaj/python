f2a8b14c49623a1ec433c8fc06100d90817dddf9
Pankaj <ydvpnkj28@gmail.com>
2025-06-06 19:33:48 +0530

    not a number question

---
+++ numpyAndPandas/eliminateNaN.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array(
+    [np.nan, 1, 2, np.nan, 3, 4, 5]
+)
+
+print("Without removing NaN from the array : ", arr)
+
+filterNaN = arr[~np.isnan(arr)]
+
+print("After removing NaN from the Array")
+print(filterNaN)
+
